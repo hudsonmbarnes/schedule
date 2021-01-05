f1b66c26c49ea6891d0beb841a761f3296c5a4e3
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-01-05 12:37:20 -0800

    started jquery function and added js script src

---
+++ javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    //Assign saveBtn click listener for user input and time stamp??
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+
