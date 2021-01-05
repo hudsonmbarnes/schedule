bcd7d0b9a551d568b4c56bc34a24f37a19052904
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-01-05 13:38:05 -0800

    get item from local storage added

--- javascript.js
+++ javascript.js
@@ -1,15 +1,23 @@
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
-    //display current day & time.
+$(document).ready(function () {
+    //display of current date and time
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    //Assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
         //get nearby values.
         console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-
-        //set items in local storage.
         localStorage.setItem(time, text);
     })
+
+// retrive data from local storage for each hour
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
